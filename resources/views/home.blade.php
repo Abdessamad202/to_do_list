32c4c1efdea226758ffc03ff51ed5d6ad902121d
abdessamad <[200~git config --global user.email kechaf.abdo.ka.2018@gmail.com>
2024-11-04 00:22:22 +0100

    adding changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,10 @@
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
+    @vite(["resources/sass/app.scss","resources/css/app.css"])
     <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" /> --}}
     <style>
         .form-control:focus {
             box-shadow: none;
