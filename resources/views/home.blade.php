a9573b323c48313c82bb336e794e34e4da2eee72
abdessamad <[200~git config --global user.email kechaf.abdo.ka.2018@gmail.com>
2024-11-03 04:40:43 +0100

    adding modifications

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
             border-radius: 0.375rem 0 0 0.375rem;
         }
 
-        .btn-primary {
+        .b {
             border-radius: 0 0.375rem 0.375rem 0;
 
         }
@@ -30,11 +30,11 @@
     <div class="container mt-4">
         <h1 class="text-center">To Do List</h1>
         <div class="input-group mb-3 container">
-            <form action="{{ route('task.add') }}" class="d-flex w-100" method="POST">
+            <form action="{{ route('task.store') }}" class="d-flex w-100" method="POST">
                 @csrf
                 <input type="text" name="task" class="form-control" placeholder="New Task ..."
                     aria-label="Recipient's username" aria-describedby="button-addon2">
-                <button class="btn btn-primary" type="submit" id="button-addon2">Add</button>
+                <button class="btn btn-primary b" type="submit" id="button-addon2">Add</button>
             </form>
         </div>
         <div class="table table-border row px-4">
@@ -45,15 +45,22 @@
                 @else
                     @foreach ($tasks as $task)
                         <div class="row mb-2 ">
-                            <div class="col-8">
-                                {{ $task->task }}
+                            <div class="col-6 ">
+                                <div class="w-100">
+                                    {{ $task->task }}
+                                </div>
                             </div>
-                            <div class="col-4">
+                            <div class="col-6">
                                 <div class="div d-flex gap-1">
+                                    <form action="{{ route('task.update', $task->id) }}" method="post">
+                                        @csrf
+                                        <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="modal"
+                                            data-bs-target="#exampleModal" data-bs-whatever="{{ $task->task }}"
+                                            data-bs-id="{{ $task->id }}">Update</button>
+                                    </form>
                                     <form action="{{ route('completed.task', $task->id) }}" method="post">
                                         @csrf
                                         <button type="submit" class="btn btn-sm btn-success">Complete</button>
-
                                     </form>
                                     <form action="{{ route('task.destroy', $task->id) }}" method="post">
                                         @method('DELETE')
@@ -91,6 +98,30 @@
             </div>
         </div>
     </div>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Update Task</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('task.update', 'x') }}" method="post">
+                        @method('PUT')
+                        @csrf
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Task:</label>
+                            <input type="text" class="form-control" name="updated_task" id="recipient-name">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
@@ -100,5 +131,34 @@
         integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
     </script>
 </body>
+<script>
+    const exampleModal = document.getElementById('exampleModal')
+    let url = "http://127.0.0.1:8000/task/"
+    if (exampleModal) {
+        // let urlStatic
+        exampleModal.addEventListener('show.bs.modal', event => {
+            // Button that triggered the modal
+            const button = event.relatedTarget
+            // Extract info from data-bs-* attributes
+            const recipient = button.getAttribute('data-bs-whatever')
+            // If necessary, you could initiate an Ajax request here
+            let id = button.getAttribute('data-bs-id')
+            // and then do the updating in a callback.
+
+            // Update the modal's content.
+            // const modalTitle = exampleModal.querySelector('.modal-title')
+            const form = exampleModal.querySelector('form')
+            const modalBodyInput = exampleModal.querySelector('#recipient-name')
+            // let url = form.getAttribute("action");
+            // url[url.length - 1] = id;
+            // if(url[url.length-1]==="k"){
+                // }
+            form.setAttribute("action",url+id)
+            // form.setAttribute("action",urlStatic+id)
+            // modalTitle.textContent = `New message to ${recipient}`;
+            modalBodyInput.value = recipient;
+        })
+    }
+</script>
 
 </html>
