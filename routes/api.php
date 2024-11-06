1b78cba11f2aae3932397166e288eebb380e131b
abdessamad <[200~git config --global user.email kechaf.abdo.ka.2018@gmail.com>
2024-11-06 02:06:26 +0100

    make the api routes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\API\TaskController;
-use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::apiResource("user",UserController::class);
+
+Route::apiResource("task",TaskController::class);
